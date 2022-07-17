29d81da0328e7520996751cdd53e93badafa2e32
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2022-07-17 04:02:27 -0300

    merge com a versao antiga

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,15 @@
-console.log ("teste");
+function toggle(id) {
+  document.getElementById('drinkContent').style.display = 'none'
+  document.getElementById('eatContent').style.display = 'none'
+  document.getElementById('eat').style.backgroundColor = 'white'
+  document.getElementById('drink').style.backgroundColor = 'white'
+  document.getElementById('eat').style.color = 'black'
+  document.getElementById('drink').style.color = 'black'
+  document.getElementById(id).style.backgroundColor = 'grey'
+  document.getElementById(id).style.color = 'white'
+  if (id === 'eat') {
+    document.getElementById('eatContent').style.display = 'initial'
+  } else {
+    document.getElementById('drinkContent').style.display = 'initial'
+  }
+}
